1f34ae5a7e71f524ff9c1d3b6477cd21f0dcde85
dymafr <erwan1309@hotmail.com>
2022-06-08 16:51:30 +0200

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
 const root = createRoot(document.getElementById('root'));
-
-setInterval(() => {
-  const element = (
-    <div>
-      <h1>Bonjour de Dyma</h1>
-      <h2>
-        Il est exactement {new Date().toLocaleTimeString()} pour ce nouvel
-        élément unique.
-      </h2>
-    </div>
-  );
-  root.render(element);
-}, 1000);
+root.render(App);
